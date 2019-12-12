3a67782d5ed54305dbb3e9d931a8ca4b45c12ecf
Mayukh Chakravartti <mayukh.chakravartti@gmail.com>
2019-12-11 23:32:49 -0500

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -25,3 +25,22 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+import pandas as pd
+
+# Read the call data into a dataframe
+df_calls = pd.DataFrame(calls, columns=['Calling_Number', 'Receiving_Number', 'Start_Time', 'Duration'])
+
+# Read the text data into a dataframe
+df_texts = pd.DataFrame(texts, columns=['Sending_Number', 'Receiving_Number', 'Timestamp'])
+
+# Find phone numbers which make calls but never receive calls
+df_tm = df_calls[~df_calls.Calling_Number.isin(df_calls.Receiving_Number)]
+
+# Cross check the above list with the phone numbers which never sends or receives texts
+df_tm = df_tm[~df_tm.Calling_Number.isin(df_texts.Sending_Number)]
+df_tm = df_tm[~df_tm.Calling_Number.isin(df_texts.Receiving_Number)]
+
+# Print the list of possible telemarketers
+print('These numbers could be telemarketers: ')
+for tm in np.sort(df_tm.Calling_Number.unique()):
+  print(tm)
